c9affa96fa6ca8c82d1dce501d2994efe71a5fea
ali21032002 <asalimi@gmail.com>
2022-12-22 21:40:23 +0330

    commiting html

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,23 @@
-<h1>
-	Test
-</h1>
+
+<script>
+	import Timer from "./Timer.svelte"; 
+	import HowTo from "./howTo.svelte"; 
+</script>
+<style>
+    h3 {
+		text-align: center;
+	}
+</style>
+
+<Timer/>
+<HowTo/>
+
+<h3>
+	<a href="https://www.who.int/gpsc/clean_hands_protection/en/">
+		Picture source
+	</a>
+
+	<a href="https://freesound.org/people/metrostock99/sounds/345086/">
+		Sound source
+	</a>
+</h3>
